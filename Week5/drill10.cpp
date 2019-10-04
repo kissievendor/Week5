1336ce034f2cc1d269a8d94a0aa95f78da582739
Kirsten Koopman <kirstenkoopman@me.com>
2019-10-04 12:47:55 +0200

    drill 2

--- Week5/drill10.cpp
+++ Week5/drill10.cpp
@@ -21,9 +21,46 @@ void Point::set_pt(double _x, double _y) {
 	y = _y;
 }
 
+vector<Point> original_pts;
+
+void fill_original_pts() {
+	cout << "Enter 7 point pairs in format x, y. \n"
+		"To end your input type x " << '\n';
+	while (original_pts.size() < 7) {
+		double x, y;
+		char comma;
+		char check;
+		cin >> check;
+
+		if (check == 'x') {
+			break;
+		}
+		else {
+			cin.putback(check);
+		}
+
+		if (cin >> x >> comma >> y) {
+			if (comma != ',') {
+				cout << "x and y seperated by comma, not: " << comma << '\n';
+			}
+			else {
+				Point pt(x, y);
+				original_pts.push_back(pt);
+			}
+		}
+		else {
+			cin.clear();
+			string check1;
+			cin >> check1;
+			cout << "Invalid value input: " << check1 << '\n';
+		}
+	}
+}
+
 int main()
 try
 {
+	fill_original_pts();
 }
 
 catch (runtime_error e) {
